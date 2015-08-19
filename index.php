a4cb9f5a582e3574ad0506a9f401c151b635d0be
Sunil Kumar <sunil.thakur@gmail.com>
2015-08-19 18:13:44 +0530

    remove unwanted lines

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 echo "<h1>I am index.php</h1>";
 echo "<h1>I am writing from web</h1>";
-echo "<h1>I am writing from web123</h1>";
-echo "<h1>I am index.php</h1>";
-echo "<h1>I am writing from web</h1>";
-echo "<h1>I am writing from web12ssss3</h1>";
 ?>
